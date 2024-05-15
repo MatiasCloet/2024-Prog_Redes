5a4d0a830e191cd6df324f449a67f47a57d08e52
MatiasCloet <101758826+MatiasCloet@users.noreply.github.com>
2024-05-15 12:47:17 -0300

    flush

--- Files/src/main/java/files/ficheros.java
+++ Files/src/main/java/files/ficheros.java
@@ -1,5 +1,47 @@
 package files;
 
-public class ficheros {
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.PrintStream;
 
+public class ficheros {
+	
+	private File archivo;
+	private PrintStream ps;
+	
+	public ficheros() 
+	{
+		String ruta = ""; // "c:\\User\\"
+		String nombre = ",mario";
+		String extencion = ".txt";
+		
+		archivo = new File( ruta.concat(nombre.concat(extencion)) );
+		
+	}
+	
+	public void createFilePrintStream(File a) 
+	{
+		
+		FileOutputStream fos = null;
+		
+		
+		try {
+
+			fos = new FileOutputStream(a);
+			ps = new PrintStream(fos,true); //append (modo de SOBRE escritura)
+											
+			ps.println("Si Si, Ñam Ñam");
+			ps.println("MAMA");
+			
+			ps.flush();
+			
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+			
+		}
+		
+		
+	}
+	
 }
