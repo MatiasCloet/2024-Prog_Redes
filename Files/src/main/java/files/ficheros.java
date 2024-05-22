a6f767719cb392c226093085e7569bdfed1454d8
MatiasCloet <101758826+MatiasCloet@users.noreply.github.com>
2024-05-22 12:02:58 -0300

    archivos

--- Files/src/main/java/files/ficheros.java
+++ Files/src/main/java/files/ficheros.java
@@ -3,7 +3,10 @@ package files;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.io.PrintStream;
+import java.io.PrintWriter;
 
 public class ficheros {
 	
@@ -20,27 +23,68 @@ public class ficheros {
 		
 	}
 	
-	public void createFilePrintStream(File a) 
+	public File getArchivo()
 	{
+		return this.archivo;
+	}
+	
+	public void createFilePrinter(File a) //clase para escribir en archivos
+	{
+		FileWriter fw = null;
+		PrintWriter pw = null; //equivalente al printStream
 		
+		try 
+		{
+			if ( !a.exists() )
+			{
+				a.createNewFile();
+			}
+			fw = new FileWriter( a , true);
+			pw = new PrintWriter(fw);
+			
+			pw.println("Otro MUNDO");		
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		finally {
+			try {
+				if( pw != null)
+					pw.close();
+				
+				if( fw != null)
+					fw.close();
+				
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			
+		}
+	}
+	
+	public void createFilePrintStream(File a)
+	{
 		FileOutputStream fos = null;
 		
-		
 		try {
-
-			fos = new FileOutputStream(a);
-			ps = new PrintStream(fos,true); //append (modo de SOBRE escritura)
-											
-			ps.println("Si Si, Ñam Ñam");
-			ps.println("MAMA");
+			fos = new FileOutputStream(a, false);
+			ps = new PrintStream(fos); //mode append true = no sobreescribe
+			
+			ps.println("holaa mundo");
+			ps.println("chau mundo");
 			
 			ps.flush();
 			
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
-			
 		}
 		
+	}
+
+	public void CreateFileBuffered(File a) 
+	{
 		
 	}
 	
--- Files/src/main/java/files/main.java
+++ Files/src/main/java/files/main.java
@@ -4,7 +4,13 @@ public class main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+		ficheros archivo = new ficheros();
+		
+		//archivo.createFilePrintStream(archivo.getArchivo());
+		archivo.createFilePrinter(archivo.getArchivo());
 	}
+	
+	
 
 }
